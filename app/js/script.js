ffe81d6e51178a1612f940f4537744c66157a147
lorsalio7 <lorsalio7@gmail.com>
2024-06-13 22:51:21 +0500

    Исправить ошибки

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,7 @@
 window.addEventListener("DOMContentLoaded", () => {
-  /*AOS.init();*/
-  /*@@include("parts/scroll-top.js");
-  @@include("parts/burger-menu.js");*/
-  /*@@include("parts/parallax-init.js");*/
+  AOS.init();
+  @@include("parts/scroll-top.js");
+  @@include("parts/burger-menu.js");
+  @@include("parts/parallax-init.js");
   @@include("parts/sliders.js");
 });
--- dist/js/script.js
+++ dist/js/script.js
@@ -1,54 +1,54 @@
 "use strict";
 
 window.addEventListener("DOMContentLoaded", () => {
-  /*AOS.init();*/
-  /*window.addEventListener("scroll", (e) => {
-  let offsetTop = window.scrollY;
-   if(offsetTop > 0) {
-    document.querySelector(".main-header").classList.add("py-3", "bg-science-blue");
-  } else {
-    document.querySelector(".main-header").classList.remove("py-3", "bg-science-blue");
-  }
-  })
-  ;
-  let burgerButton = document.querySelector(".burger-button");
-  if(burgerButton) {
-  let burgerMenuWidth = window.matchMedia("(max-width: 768px)");
-  let burgerButtonLine = burgerButton.querySelector(".burger-button-line");
-  let burgerButtonLineClassList = Array.from(burgerButtonLine.classList).join(" ");
-  let siteNavigation = document.querySelector(".site-navigation");
-   burgerButton.addEventListener("click", () => {
-    burgerButton.classList.toggle("burger-button--active");
-     if(burgerButton.classList.contains("burger-button--active")) {
-      openSiteMenu();
+  AOS.init();
+  window.addEventListener("scroll", e => {
+    let offsetTop = window.scrollY;
+    console.log("work");
+    if (offsetTop > 0) {
+      document.querySelector(".main-header").classList.add("py-3", "bg-science-blue");
     } else {
-      closeSiteMenu();
+      document.querySelector(".main-header").classList.remove("py-3", "bg-science-blue");
     }
-  })
-   function openSiteMenu() {
-    burgerButtonLine.className = "burger-button-line absolute left-1/2 top-1/2 block w-[34px] h-[2px] -translate-x-1/2 -translate-y-1/2 transition-colors duration-300 delay-300 ease-linear bg-transparent before:content-[''] before:absolute before:block before:w-[34px] before:h-[2px] before:bg-white before:left-0 before:top-0 before:rotate-45 before:transition-[top,transform] before:duration-[0.3s,0.3s] before:delay-[0s,0.3s] before:ease-[linear,linear] after:content-[''] after:absolute after:left-0 after:top-0 after:-rotate-45 after:block after:w-[34px] after:h-[2px] after:bg-white after:transition-[top,transform] after:duration-[0.3s,0.3s] after:delay-[0s,0.3s] after:ease-linear";
-    siteNavigation.className = "site-navigation flex justify-between md:fixed md:top-0 md:bottom-0 md:right-0 md:h-full md:flex-col md:bg-science-blue md:w-full md:transition-transform md:duration-300 md:ease-linear md:translate-x-0 md:justify-center md:py-[86px] md:items-center";
-  }
-   function closeSiteMenu() {
-    burgerButton.classList.remove("burger-button--active");
-    burgerButtonLine.className = burgerButtonLineClassList;
-    siteNavigation.className = "site-navigation flex justify-between md:fixed md:top-0 md:bottom-0 md:right-0 md:h-full md:flex-col md:bg-science-blue md:w-full md:transition-transform md:duration-300 md:ease-linear md:-translate-x-[100%] md:justify-center md:py-[86px] md:items-center";
-  }
-   function changeMenuView(width) {
-    if(!width) {
-      closeSiteMenu();
-    }
-  }
-   burgerMenuWidth.onchange = (e) => {
-    changeMenuView(e.matches);
-  }
-  }
-  ;*/
-  /*let rellax = new Rellax('.rellax', {
-  wrapper: null,
-  vertical: true,
   });
-  ;*/
+  let burgerButton = document.querySelector(".burger-button");
+  if (burgerButton) {
+    let burgerMenuWidth = window.matchMedia("(max-width: 768px)");
+    let burgerButtonLine = burgerButton.querySelector(".burger-button-line");
+    let burgerButtonLineClassList = Array.from(burgerButtonLine.classList).join(" ");
+    let siteNavigation = document.querySelector(".site-navigation");
+    burgerButton.addEventListener("click", () => {
+      burgerButton.classList.toggle("burger-button--active");
+      if (burgerButton.classList.contains("burger-button--active")) {
+        openSiteMenu();
+      } else {
+        closeSiteMenu();
+      }
+    });
+    function openSiteMenu() {
+      burgerButtonLine.className = "burger-button-line absolute left-1/2 top-1/2 block w-[34px] h-[2px] -translate-x-1/2 -translate-y-1/2 transition-colors duration-300 delay-300 ease-linear bg-transparent before:content-[''] before:absolute before:block before:w-[34px] before:h-[2px] before:bg-white before:left-0 before:top-0 before:rotate-45 before:transition-[top,transform] before:duration-[0.3s,0.3s] before:delay-[0s,0.3s] before:ease-[linear,linear] after:content-[''] after:absolute after:left-0 after:top-0 after:-rotate-45 after:block after:w-[34px] after:h-[2px] after:bg-white after:transition-[top,transform] after:duration-[0.3s,0.3s] after:delay-[0s,0.3s] after:ease-linear";
+      siteNavigation.className = "site-navigation flex justify-between md:fixed md:top-0 md:bottom-0 md:right-0 md:h-full md:flex-col md:bg-science-blue md:w-full md:transition-transform md:duration-300 md:ease-linear md:translate-x-0 md:justify-center md:py-[86px] md:items-center";
+    }
+    function closeSiteMenu() {
+      burgerButton.classList.remove("burger-button--active");
+      burgerButtonLine.className = burgerButtonLineClassList;
+      siteNavigation.className = "site-navigation flex justify-between md:fixed md:top-0 md:bottom-0 md:right-0 md:h-full md:flex-col md:bg-science-blue md:w-full md:transition-transform md:duration-300 md:ease-linear md:-translate-x-[100%] md:justify-center md:py-[86px] md:items-center";
+    }
+    function changeMenuView(width) {
+      if (!width) {
+        closeSiteMenu();
+      }
+    }
+    burgerMenuWidth.onchange = e => {
+      changeMenuView(e.matches);
+    };
+  }
+  ;
+  let rellax = new Rellax('.rellax', {
+    wrapper: null,
+    vertical: true
+  });
+  ;
   let toursSlider = document.querySelector(".tours-slider");
   let trainersSlider = document.querySelector(".trainers-slider");
   let reviewsSlider = document.querySelector(".reviews-slider");
