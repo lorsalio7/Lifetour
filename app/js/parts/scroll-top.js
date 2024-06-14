2be7f080843b5dd7ec84bc589ac9af3dc7b77810
lorsalio7 <lorsalio7@gmail.com>
2024-06-14 23:27:44 +0500

    Добавить якоря навигации

--- app/js/parts/scroll-top.js
+++ app/js/parts/scroll-top.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", (e) => {
   let offsetTop = window.scrollY;
-  console.log("work");
+
   if(offsetTop > 0) {
     document.querySelector(".main-header").classList.add("py-3", "bg-science-blue");
   } else {
